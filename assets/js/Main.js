e5f073804033d2fab658ce86a721c1a8f2c85e20
Digiworksolution <139655640+Digiworksolution@users.noreply.github.com>
2023-08-20 13:08:00 +0530

    Js changes

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -12,78 +12,78 @@ function analysisSubmit() {
     }
 }
 
-const analysisData = () => {
+// const analysisData = () => {
 
-    const { value: name } = document.getElementById("name");
-    const { value: number } = document.getElementById("number");
-    const { value: email } = document.getElementById("email");
-    const { value: smm } = document.getElementById("smm");
-    const { value: website } = document.getElementById("websites");
-    const { value: message } = document.getElementById("message");
+//     const { value: name } = document.getElementById("name");
+//     const { value: number } = document.getElementById("number");
+//     const { value: email } = document.getElementById("email");
+//     const { value: smm } = document.getElementById("smm");
+//     const { value: website } = document.getElementById("websites");
+//     const { value: message } = document.getElementById("message");
 
-    console.log(`Name: ${name}`);
-    console.log(`Contact No: ${number}`);
-    console.log(`Email: ${email}`);
-    console.log(`Social Media Handle Link: ${smm}`);
-    console.log(`Website: ${website}`);
-    console.log(`Message: ${message}`);
+//     console.log(`Name: ${name}`);
+//     console.log(`Contact No: ${number}`);
+//     console.log(`Email: ${email}`);
+//     console.log(`Social Media Handle Link: ${smm}`);
+//     console.log(`Website: ${website}`);
+//     console.log(`Message: ${message}`);
 
 
-    alert("Your data is successfully submitted!");
-
-    
-    document.getElementById("analysisform").reset();
+//     alert("Your data is successfully submitted!");
 
     
-    return false;
-};
+//     document.getElementById("analysisform").reset();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDm6Iwdakaqap_p0gsWzM17_t3hPWu9m1A",
-  authDomain: "digiwork-1c46b.firebaseapp.com",
-  databaseURL: "https://digiwork-1c46b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "digiwork-1c46b",
-  storageBucket: "digiwork-1c46b.appspot.com",
-  messagingSenderId: "580115971656",
-  appId: "1:580115971656:web:c595065b0954e8a803e27d",
-  measurementId: "G-EB3TXJJ1PJ"
-};
+    
+//     return false;
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDm6Iwdakaqap_p0gsWzM17_t3hPWu9m1A",
+//   authDomain: "digiwork-1c46b.firebaseapp.com",
+//   databaseURL: "https://digiwork-1c46b-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "digiwork-1c46b",
+//   storageBucket: "digiwork-1c46b.appspot.com",
+//   messagingSenderId: "580115971656",
+//   appId: "1:580115971656:web:c595065b0954e8a803e27d",
+//   measurementId: "G-EB3TXJJ1PJ"
+// };
 
-// Get a reference to the Firestore database
-const db = firebase.firestore();
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-// Get a reference to the contact form
-const contactForm = document.getElementById('contactForm');
+// // Get a reference to the Firestore database
+// const db = firebase.firestore();
 
-// Listen for the form submission
-contactForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
+// // Get a reference to the contact form
+// const contactForm = document.getElementById('contactForm');
 
-    // Get form data
-    const formData = new FormData(contactForm);
-    const name = formData.get('name');
-    const contact = formData.get('contact');
-    const email = formData.get('email');
-    const message = formData.get('message');
+// // Listen for the form submission
+// contactForm.addEventListener('submit', (event) => {
+//     event.preventDefault(); // Prevent default form submission behavior
 
-    // Save the form data to Firestore
-    db.collection('contact-submissions').add({
-        name: name,
-        contact: contact,
-        email: email,
-        message: message
-    })
-    .then(() => {
-        // Form submission successful
-        alert('Thank you for your submission!');
-        contactForm.reset();
-    })
-    .catch((error) => {
-        // Form submission failed
-        console.error('Error submitting form:', error);
-        alert('There was an error submitting the form. Please try again.');
-    });
-});
+//     // Get form data
+//     const formData = new FormData(contactForm);
+//     const name = formData.get('name');
+//     const contact = formData.get('contact');
+//     const email = formData.get('email');
+//     const message = formData.get('message');
+
+//     // Save the form data to Firestore
+//     db.collection('contact-submissions').add({
+//         name: name,
+//         contact: contact,
+//         email: email,
+//         message: message
+//     })
+//     .then(() => {
+//         // Form submission successful
+//         alert('Thank you for your submission!');
+//         contactForm.reset();
+//     })
+//     .catch((error) => {
+//         // Form submission failed
+//         console.error('Error submitting form:', error);
+//         alert('There was an error submitting the form. Please try again.');
+//     });
+// });
